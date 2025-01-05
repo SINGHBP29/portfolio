796e0b2d21a719e203e2166468c2699f33abb830
Bhanu Pratap Singh <98203615+SINGHBP29@users.noreply.github.com>
2025-01-06 01:30:29 +0530

    Update settings.py

--- Django_Portfilio_MohammadBurhan/settings.py
+++ Django_Portfilio_MohammadBurhan/settings.py
@@ -115,11 +115,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For collected static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')  # For your local static files
+]
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 
