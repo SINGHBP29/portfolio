a55f553a2acf18f9e9752a8da08cd9bdce1aa21b
Bhanu Pratap Singh <98203615+SINGHBP29@users.noreply.github.com>
2025-01-06 20:24:39 +0530

    Update wsgi.py

--- Django_Portfilio_MohammadBurhan/wsgi.py
+++ Django_Portfilio_MohammadBurhan/wsgi.py
@@ -12,8 +12,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+# application = StaticFilesHandler(get_wsgi_application())
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_Portfilio_MohammadBurhan.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
